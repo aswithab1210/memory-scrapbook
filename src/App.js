703e657031caa176054ddc830b3d6659af99bc5c
SAI ASWITHA <aswithakousi@gmail.com>
2025-04-18 17:19:57 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <TopNav/>
+      </div>
       <TodoList />
     </div>
   );
